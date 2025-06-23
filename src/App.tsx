8e914efe7f1b7552c75180d130ff9ca2f79b18bb
Ricardo Cardoso <ricardomanolo90@gmail.com>
2025-06-23 15:36:47 -0300

    update commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import './App.css';
-import './components/left-side/styles.css';
-import './components/right-side/styles.css';
+import "./App.css";
+import { useState } from "react";
 
-import { useState } from 'react';
-import { calcImc } from './components/calc-imc';
-import { levels, level } from './components/calc-imc';
-import { GridItem } from './components/grid-item';
+import "./styles/left-side/styles.css";
+import "./styles/right-side/styles.css";
+
+import { calcImc } from "./components/calc-imc";
+import { GridItem } from "./components/grid-item";
+import { levels, level } from "./components/calc-imc";
 
 function App() {
-
   const [heightField, setHeightField] = useState<number>(0);
   const [weightField, setWeightField] = useState<number>(0);
   const [toShow, setToShow] = useState<level | null>(null);
 
   const imgPowered = require("./assets/powered.png") as string;
-  const leftArrow = require('./assets/leftarrow.png') as string;
+  const leftArrow = require("./assets/leftarrow.png") as string;
 
   const handleHeightField = (e: React.ChangeEvent<HTMLInputElement>) => {
     setHeightField(parseFloat(e.target.value));
   };
 
   const handleWeightField = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setWeightField(parseFloat(e.target.value))
+    setWeightField(parseFloat(e.target.value));
   };
 
   const handleCalcButton = () => {
     if (heightField && weightField) {
       setToShow(calcImc(heightField, weightField));
     } else {
-      alert('Algo de errado não está certo!!');
+      alert("Algo de errado não está certo!!");
     }
   };
 
@@ -39,61 +39,68 @@ function App() {
   };
 
   return (
-    <div className='box'>
-      <div className='img-box'>
+    <div className="box">
+      <div className="img-box">
         <img src={imgPowered} alt="" width={90} />
       </div>
-      <div className='container'>
-
-        <div className='left-side'>
+      <div className="container">
+        <div className="left-side">
           <h1>Calcule o seu IMC.</h1>
-          <p>IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela Organização Mundial da Saúde para calcular o peso ideal para cada pessoa.</p>
+          <p>
+            IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela
+            Organização Mundial da Saúde para calcular o peso ideal para cada
+            pessoa.
+          </p>
 
-          <input className='height'
+          <input
+            className="height"
             type="number"
-            value={heightField > 0 ? heightField : ''}
+            value={heightField > 0 ? heightField : ""}
             onChange={handleHeightField}
-            placeholder='Digite a sua altura. Ex: 1.5 (em métros).'
+            placeholder="Digite a sua altura. Ex: 1.5 (em métros)."
             disabled={toShow ? true : false}
           />
 
-          <input className='weight'
+          <input
+            className="weight"
             type="number"
-            value={weightField > 0 ? weightField : ''}
+            value={weightField > 0 ? weightField : ""}
             onChange={handleWeightField}
-            placeholder='Digite o seu peso. Ex: 75.3 (em kg).'
+            placeholder="Digite o seu peso. Ex: 75.3 (em kg)."
             disabled={toShow ? true : false}
           />
 
-          <button onClick={handleCalcButton}
-            disabled={toShow ? true : false}
-          >Calcular</button>
+          <button onClick={handleCalcButton} disabled={toShow ? true : false}>
+            Calcular
+          </button>
         </div>
 
-
-        <div className='right-side'>
-          {!toShow &&
-            <div className='grid'>
+        <div className="right-side">
+          {!toShow && (
+            <div className="grid">
               {levels.map((item, key) => {
-                return (
-                  <GridItem key={key} item={item} />
-                )
+                return <GridItem key={key} item={item} />;
               })}
             </div>
-          }
+          )}
 
-          {toShow &&
-            <div className='rightBig'>
-              <div className='rightArrow'>
-                <img src={leftArrow} alt="" width={25} onClick={handleBackButton} />
+          {toShow && (
+            <div className="rightBig">
+              <div className="rightArrow">
+                <img
+                  src={leftArrow}
+                  alt=""
+                  width={25}
+                  onClick={handleBackButton}
+                />
               </div>
               <GridItem item={toShow} />
             </div>
-          }
+          )}
         </div>
       </div>
     </div>
-  )
-};
+  );
+}
 
 export default App;
--- src/components/grid-item/index.tsx
+++ src/components/grid-item/index.tsx
@@ -1,30 +1,31 @@
-import '../right-side/styles.css';
+import "../right-side/styles.css";
 import { level } from "../calc-imc";
 
-import upImage from '../../assets/up.png';
-import downImage from '../../assets/down.png';
+import upImage from "../../assets/up.png";
+import downImage from "../../assets/down.png";
 
 type Props = {
-    item: level;
+  item: level;
 };
 
 export const GridItem = ({ item }: Props) => {
-    return (
-        <div className='main' style={{ backgroundColor: item.color }}>
-            <div className='gridIcon'>
-                <img src={item.icon === 'up' ? upImage : downImage} alt="" width={30} />
-            </div>
-            <div className='gridTitle'>{item.title}</div>
+  return (
+    <div className="main" style={{ backgroundColor: item.color }}>
+      <div className="gridIcon">
+        <img src={item.icon === "up" ? upImage : downImage} alt="" width={30} />
+      </div>
+      <div className="gridTitle">{item.title}</div>
 
-            {item.yourImc &&
-                <div className='yourImc'>Seu IMC é de {item.yourImc} kg/m.</div>
-            }
+      {item.yourImc && (
+        <div className="yourImc">Seu IMC é de {item.yourImc} kg/m.</div>
+      )}
 
-            <div className='gridInfo'>
-                {/* <> */}
-                IMC está entre <strong>{item.imc[0]}</strong> e <strong>{item.imc[1]}</strong>
-                {/* </> */}
-            </div>
-        </div>
-    );
+      <div className="gridInfo">
+        {/* <> */}
+        IMC está entre <strong>{item.imc[0]}</strong> e{" "}
+        <strong>{item.imc[1]}</strong>
+        {/* </> */}
+      </div>
+    </div>
+  );
 };
